d8caf811bd695c8f4eecda46a50a142f3ce1e5bc
emrepl31 <85391278+emrepl31@users.noreply.github.com>
2021-06-15 16:50:23 +0300

    Add files via upload

---
+++ Tool-Download.sh
@@ -0,0 +1,22 @@
+echo " "
+echo " "
+echo " "
+echo "
+╱╭╮╱╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╭╮
+╭╯╰╮╱╱╱╱╱┃┃╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱┃┃
+╰╮╭╋━━┳━━┫┃╱╭━╯┣━━┳╮╭╮╭┳━╮┃┃╭━━┳━━┳━╯┃
+╱┃┃┃╭╮┃╭╮┃┃╱┃╭╮┃╭╮┃╰╯╰╯┃╭╮┫┃┃╭╮┃╭╮┃╭╮┃
+╱┃╰┫╰╯┃╰╯┃╰╮┃╰╯┃╰╯┣╮╭╮╭┫┃┃┃╰┫╭╮┃╰╯┃╰╯┃
+╱╰━┻━━┻━━┻━╯╰━━┻━━╯╰╯╰╯╰╯╰┻━┻╯╰┻━━┻━━╯BETA VERSİON "|lolcat
+echo " "
+echo " 
+[1] Ddoser Tool "|lolcat
+echo " 
+<─────[ What Do Use ]─────> "|lolcat
+echo -en "\e[93m #: \e[m"
+read answer
+echo "
+<───[ Okey Installing Please Wait. ]───> "|lolcat
+git clone https://github.com/cyweb/hammer
+echo "
+<──[ Done Installing ]──> "|lolcat
